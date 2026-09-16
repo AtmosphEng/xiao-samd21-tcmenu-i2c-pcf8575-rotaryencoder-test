4e6afb36b152bf95ea96bb3308a6fe7f74e92a82
atmospheng <atmospheng@gmail.com>
2026-09-16 22:56:46 +0800

    Updated tcmDesigner emf files to include I2C interrupt and polling versions.

--- src/generated/xiao-samd21-tcmenu-i2c-rotaryencoder-test_menu.h
+++ src/generated/xiao-samd21-tcmenu-i2c-rotaryencoder-test_menu.h
@@ -25,8 +25,8 @@ extern U8g2Drawable gfxDrawable;
 extern GraphicsDeviceRenderer renderer;
 
 // Any externals needed by IO expanders, EEPROMs etc
-extern IoAbstractionRef ioexp_my8575Expansion;
 extern IoAbstractionRef ioexp_myI2CExpansion;
+extern IoAbstractionRef ioexp_my8575Expansion;
 
 // Global Menu Item exports
 extern AnalogMenuItem menuTcmUpSeconds;
